be14b440da1f8ea5d2a101659d28272b26e0edb2
linder3hs <linderhassinger00@gmail.com>
2021-09-29 13:15:42 -0500

    Initial Commit

---
+++ semana7-b/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  semana7-b
+//
+//  Created by Linder Hassinger on 29/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
